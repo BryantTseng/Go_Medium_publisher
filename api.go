4cba517a2f308b90dccba4e480cf117cbcc3fee2
Bryant Tseng <bryant5968@gmail.com>
2020-05-05 00:28:01 +0900

    add user profile

--- api.go
+++ api.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 )
 
 // APIHandler is a handler for calling api
 type APIHandler struct {
 	Token string
+}
+type UserProfile struct {
+	Data ProfileDetail
+}
+type ProfileDetail struct {
+	Id       string
+	Username string
+	Name     string
+	Url      string
+	ImageUrl string
 }
 
 // ReadConfig reads config from file. etc. medium access token
@@ -23,4 +35,26 @@ func (a *APIHandler) ReadConfig() {
 	data, _ := ioutil.ReadAll(file)
 	json.Unmarshal(data, &a)
 
+}
+func (a APIHandler) GetUserDetail() {
+	api := "https://api.medium.com/v1/me"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", api, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Add("Authorization", "Bearer "+a.Token)
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if resp.StatusCode == 401 {
+		// token not authorized
+		log.Fatalln("Token was invalid.")
+	}
+	var up UserProfile
+	body, err := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(body, &up)
+	fmt.Printf(string(body))
 }
--- main.go
+++ main.go
@@ -3,4 +3,5 @@ package main
 func main() {
 	a := APIHandler{}
 	a.ReadConfig()
+	a.GetUserDetail()
 }
